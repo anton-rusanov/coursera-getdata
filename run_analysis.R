c1f4f323772abd1a57147833b8112186fe2593d5
Anton Rusanov <anton.d.rusanov@gmail.com>
2015-02-20 23:44:17 -0800

    Added comments to the code blocks.

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 run.analysis <- function(download.data = FALSE) {
   print("Started")
+  # Maybe download and unzip the archive.
   if (download.data) {
     originalDatasetUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
     tempZipFile <- tempfile(pattern="UCI-HAR-Dataset", tmpdir = ".", fileext = ".zip")
@@ -10,11 +11,13 @@ run.analysis <- function(download.data = FALSE) {
     print("Unzipped archive")
   } 
   
+  # Extract only the measurements on the mean and standard deviation for each measurement. 
   features <- read.table("UCI HAR Dataset/features.txt")
   meanStdFeatureIndices <- grep("-mean\\(\\)|-std\\(\\)", features$V2)
   meanStdFeatureNames<- grep("-mean\\(\\)|-std\\(\\)", features$V2, value = TRUE)
   print("Got -mean() and -std() features' indices and names")
   
+  # Merge the variables from training and the test sets to create one data set.
   print("Reading test data...")
   dataTest <- read.table("UCI HAR Dataset/test/X_test.txt")
   print("Read test data")
@@ -24,25 +27,31 @@ run.analysis <- function(download.data = FALSE) {
   print("Merged data")
   dataMeanAndStd <- dataMerged[, meanStdFeatureIndices]
   
+  # Merge the labels from training and the test sets to create one data set.
   print("Reading labels...")
   labelsTest <- read.table("UCI HAR Dataset/test/y_test.txt")
   labelsTrain <- read.table("UCI HAR Dataset/train/y_train.txt")
   labelsMerged <- rbind(labelsTest, labelsTrain)
   print("Read and merged labels")
   
+  # Use descriptive activity names to name the activities in the data set.
   activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
   labelsMerged[, "prettyLabel"] = activityLabels[labelsMerged$V1,c('V2')]
   print("Added pretty names to labels")
   
+  # Merge the subjects from training and the test sets to create one data set.
   subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
   subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
   subjectMerged <- rbind(subjectTest, subjectTrain)
   print("Read and merged subjects")
   
+  # Merge mean and standard deviation columns, subjects and descriptive names for labels in one dataset.
   dataLabels <- cbind(dataMeanAndStd, subjectMerged, labelsMerged$prettyLabel)
   colnames(dataLabels) <- c(meanStdFeatureNames, "subject", "activity_label")
   print("Set readable column names")
-  
+ 
+  # From the data set above, create a second, independent tidy data set with the average of each variable 
+  # for each activity and each subject.
   meltedData = melt(dataLabels, id = c("subject", "activity_label"))
   tidyData <- dcast(meltedData, formula = ...  ~ variable, mean)
   print("Calculated means")
